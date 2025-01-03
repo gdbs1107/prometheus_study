9ba0720cb9fa9e423d49dc7229b7117c8f06ac3e
gdbs1107 <gdbs1107@naver.com>
2025-01-03 20:51:05 +0900

    prometheus data 파일 직접 생성

--- src/main/java/com/example/springacuatortest/controller/HelloController.java
+++ src/main/java/com/example/springacuatortest/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    private int i = 0; // 멤버 변수로 i를 선언하여 값을 유지
+    private int i = 0; // 멤버 변수로 i를 선언하여 값을 유지1
 
     @GetMapping("/hello")
     public String hello() {
